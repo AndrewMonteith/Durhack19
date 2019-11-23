d9927fce18a36670643a9ab1281669ba53e47c80
Laurie <laurence.ho@durham.ac.uk>
2019-11-23 16:24:55 +0000

    start logins

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const express = require('express');
+const login = require('./login');
+const twilio = require('./twilio');
+
+const app = express();
+
+app.use('/message', twilio);
+app.use('/login', login);
+
+
+app.get('/', (req, res) => {
+    return 'hi';
+});
+app.listen(5000);
