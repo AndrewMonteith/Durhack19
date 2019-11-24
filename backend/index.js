62fdbdb08ac1afb5257f9bb68d761056fcbd7951
AndrewMonteith <andrewmonteith1999@gmail.com>
2019-11-24 04:49:19 +0000

    Fixed

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require('express')
 const cors = require("cors")
 
-const signup = require('./signup');
-const login = require('./login');
+const signup = require('./signup')
+const login = require('./login')
 const search = require('./search')
+const users = require('./users')
+const twilio = require('./twilio')
 
-const app = express();
+const app = express()
 
 app.use(cors())
-app.use('/signup', signup);
-app.use('/login', login);
+app.use('/signup', signup)
+app.use('/login', login)
 app.use('/search', async (request, response) => {
     request.header("Access-Control-Allow-Origin")
 
@@ -19,16 +21,43 @@ app.use('/search', async (request, response) => {
         search: request.query.search
     }
 
+    
+
     const result = await search(query)
    
+    console.log("Returning result to client", result, " based on ", query)
+
     response.status(200)
-    console.log("giving back", result)
     response.json(result)
+})
+
+app.use('/message', (request, response) => {
+    request.header("Access-Control-Allow-Origin")
+
+    const requestBlob = request.query.blob
+    const numberFrom = request.query.fromNumber;
+    const requestMessage = request.query.message
+
+    let numberTo
+    if (typeof(requestBlob.isCharity) === 'boolean') {
+        numberTo = requestBlob.phoneNumber
+    } else {
+        // Look in users for who is running the meetup
+        for (const user of users) {
+            if (user.user === requestBlob.who) {
+                numberTo = requestBlob.who
+                break 
+            }
+        }
+    }
+
+    twilio.sendMessage(numberFrom, numberTo, requestMessage)
+
 })
 
 
 app.get('/', (req, res) => {
-    res.json();
-});
+    res.json()
+})
 
-app.listen(5000);
+app.listen(5000)
